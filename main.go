2c32f85f5e78b98da752399edae2366bc2262554
pengenbisacoding <94414447+pengenbisacoding@users.noreply.github.com>
2023-09-22 15:35:43 +0700

    Tugas Camp 2
    Muhammad Harits Zulkifli
    Pertemuan 2 , Minggu jam 08.00 - Selesai

--- main.go
+++ main.go
@@ -2,80 +2,30 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"sort"
 )
 
 func main() {
-	fmt.Println("Program Menghitung Luas Persegi, Lingkara, Segitiga")
+	numbers := []int{5, 9, 3, 1, 7} // Data bilangan yang akan diurutkan
 
-	// Menghitung luas persegi
-	sisiPersegi := 5.0                      // Menginisialisasi variabel sisiPersegi dengan nilai 5.0
-	luasPersegi := math.Pow(sisiPersegi, 2) // Menghitung luas persegi dengan menggunakan fungsi math.Pow untuk mengkuadratkan sisiPersegi
-	fmt.Printf("Luas Persegi dengan sisi %.2f adalah %.2f\n", sisiPersegi, luasPersegi)
+	fmt.Println("Sebelum diurutkan:", numbers)
 
-	// Menghitung luas lingkaran
-	jariJari := 4.0                                  // Menginisialisasi variabel jariJari dengan nilai 4.0
-	luasLingkaran := math.Pi * math.Pow(jariJari, 2) // Menghitung luas lingkaran dengan menggunakan konstanta math.Pi dan fungsi math.Pow untuk mengkuadratkan jariJari
-	fmt.Printf("Luas Lingkaran dengan jari-jari %.2f adalah %.2f\n", jariJari, luasLingkaran)
+	sort.Sort(sort.Reverse(sort.IntSlice(numbers)))
 
-	// Menghitung luas segitiga
-	alas := 6.0                         // Menginisialisasi variabel alas dengan nilai 6.0
-	tinggi := 8.0                       // Menginisialisasi variabel tinggi dengan nilai 8.0
-	luasSegitiga := 0.5 * alas * tinggi // Menghitung luas segitiga dengan perkalian 0.5, alas, dan tinggi
-	fmt.Printf("Luas Segitiga dengan alas %.2f dan tinggi %.2f adalah %.2f\n", alas, tinggi, luasSegitiga)
-	// D. Menghitung Energi Potensial dan Kinetik
-	fmt.Println("Program Menghitung Energi Potensial dan Kinetik")
-	// Menghitung energi potensial
-	massa := 5.0       // Massa benda dalam kg
-	gravitasi := 9.8   // Percepatan gravitasi dalam m/s^2
-	ketinggian := 10.0 // Ketinggian benda dalam meter
+	fmt.Println("Setelah diurutkan:", numbers)
 
-	energiPotensial := massa * gravitasi * ketinggian
-	fmt.Printf("Energi Potensial: %.2f Joule\n", energiPotensial)
+	// Membuat slice dengan jumlah data 5
+	slice := make([]int, 5)
 
-	// Menghitung energi kinetik
-	kecepatan := 15.0 // Kecepatan benda dalam m/s
+	// Menambahkan data ke dalam slice sebanyak 3
+	for i := 0; i < 3; i++ {
+		var data int
+		fmt.Printf("Masukkan data ke-%d: ", i+1)
+		fmt.Scan(&data)
+		slice = append(slice, data)
+	}
 
-	energiKinetik := 0.5 * massa * kecepatan * kecepatan
-	fmt.Printf("Energi Kinetik: %.2f Joule\n", energiKinetik)
-
-	// E. Menghitung frekuensi atau getaran
-
-	fmt.Println("Program Menghitung Frekuensi atau Getaran")
-
-	// Input periode dalam detik
-	var periode float64
-	fmt.Print("Masukkan periode dalam detik: ")
-	fmt.Scanln(&periode)
-
-	// Menghitung frekuensi
-	frekuensi := 1 / periode
-	fmt.Printf("Frekuensi: %.2f Hz\n", frekuensi)
-
-	// Menghitung jumlah getaran dalam satu menit
-	jumlahGetaran := 60 / periode
-	fmt.Printf("Jumlah Getaran dalam Satu Menit: %.2f\n", jumlahGetaran)
-
-	fmt.Println("Program Konversi Suhu")
-	// F. Konversi untuk semua satuan suhu
-	// Input suhu dalam Celcius
-	var suhuCelcius float64
-	fmt.Print("Masukkan suhu dalam Celcius: ")
-	fmt.Scanln(&suhuCelcius)
-
-	// Konversi ke Fahrenheit
-	suhuFahrenheit := (suhuCelcius * 9 / 5) + 32
-	fmt.Printf("Suhu dalam Fahrenheit: %.2f\n", suhuFahrenheit)
-
-	// Konversi ke Kelvin
-	suhuKelvin := suhuCelcius + 273.15
-	fmt.Printf("Suhu dalam Kelvin: %.2f\n", suhuKelvin)
-
-	// Konversi ke Reamur
-	suhuReamur := suhuCelcius * 4 / 5
-	fmt.Printf("Suhu dalam Reamur: %.2f\n", suhuReamur)
-
-	// Konversi ke Rankine
-	suhuRankine := (suhuCelcius + 273.15) * 9 / 5
-	fmt.Printf("Suhu dalam Rankine: %.2f\n", suhuRankine)
+	// Menampilkan isi slice
+	fmt.Println("Isi slice:")
+	fmt.Println(slice)
 }
